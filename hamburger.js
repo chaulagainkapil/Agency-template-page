5a316ee6d3d59f5647a1510dea81db4005d146d5
chaulagainkapil <chaukapil99@gmail.com>
2023-04-11 09:40:06 +0545

    seperate js file

---
+++ hamburger.js
@@ -0,0 +1,5 @@
+const menu = document.getElementById("navlinks");
+      const hamburger = document.getElementById("hamburger");
+      function show() {
+        menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+      }
